0019bb4f627b3ba12e9b96ed09fbe75bc8501d4a
hwhh <hwhh@st-andrews.ac.uk>
2017-03-28 16:24:22 +0100

    Updated ignore

--- Main/basic_1.py
+++ Main/basic_1.py
@@ -1,13 +1,10 @@
+import pandas as pd
+import Matplotlib
 
 def readCsv():
-    with open('data', 'r') as f:
-        headers = f.readline()
-        headers = headers.split(',')
-        print(headers[0])
+    data = pd.read_csv("data")
 
-        for line in f:
-            line = line.split(',')
-            check(line)
+
 
 def check(vals):
     if len(vals) != 18:
@@ -21,3 +18,18 @@ def check(vals):
     if (vals[3] != -9 and vals[3] < 1) or vals[3] > 5:
         return False
     if vals[4] != -9
+
+
+
+
+
+
+
+    # with open('data', 'r') as f:
+    #     headers = f.readline()
+    #     headers = headers.split(',')
+    #     print(headers[0])
+    #
+    #     for line in f:
+    #         line = line.split(',')
+    #         check(line)
