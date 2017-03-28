4fc8e824d03fa37627588d813c00cf47b27e0d10
hwhh <hwhh@st-andrews.ac.uk>
2017-03-28 17:31:57 +0100

    Completed first basic

--- Main/basic_1.py
+++ Main/basic_1.py
@@ -1,35 +1,27 @@
 import pandas as pd
-import Matplotlib
+
 
 def readCsv():
-    data = pd.read_csv("data")
+    data = pd.read_csv("data", error_bad_lines=False)
+    data = data[data["Region"].isin(["E12000001", "E12000002", "E12000003", "E12000004", "E12000005", "E12000006", "E12000007", "E12000008", "E12000009", "W92000004"])]
+    data = data[data["Residence Type"].isin(['C', 'H'])]
+    data = data[data["Family Composition"].isin([1, 2, 3, 4, 5, 6, -9])]# can change to: list(range(0, x).append(-9)
+    data = data[data["Population Base"].isin([1, 2, 3])]
+    data = data[data["Sex"].isin([1, 2])]
+    data = data[data["Age"].isin([1, 2, 3, 4, 5, 6, 7, 8])]
+    data = data[data["Marital Status"].isin([1, 2, 3, 4, 5])]
+    data = data[data["Student"].isin([1, 2])]
+    data = data[data["Country of Birth"].isin([1, 2, -9])]
+    data = data[data["Health"].isin([1, 2, 3, 4, 5, -9])]
+    data = data[data["Ethnic Group"].isin([1, 2, 3, 4, 5, -9])]
+    data = data[data["Religion"].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
+    data = data[data["Economic Activity"].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
+    data = data[data["Occupation"].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
+    data = data[data["Industry"].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, -9])]
+    data = data[data["Hours worked per week"].isin([1, 2, 3, 4, -9])]
+    data = data[data["Approximated Social Grade"].isin(["AB", "C1", "C2", "DE", -9])]
+    print(data)
 
 
 
-def check(vals):
-    if len(vals) != 18:
-        return False
-    if vals[0] < 1 or vals[0] > 5:
-        return False
-    if vals[1] < 1 or vals[1] > 2:
-        return False
-    if (vals[2] != -9 and vals[2] < 1) or vals[2] > 2:
-        return False
-    if (vals[3] != -9 and vals[3] < 1) or vals[3] > 5:
-        return False
-    if vals[4] != -9
-
-
-
-
-
-
-
-    # with open('data', 'r') as f:
-    #     headers = f.readline()
-    #     headers = headers.split(',')
-    #     print(headers[0])
-    #
-    #     for line in f:
-    #         line = line.split(',')
-    #         check(line)
+readCsv()
