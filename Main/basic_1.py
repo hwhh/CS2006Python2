ec8f416a92b08b767d94901d253e297ecd9f111e
hwhh <hwhh@st-andrews.ac.uk>
2017-03-31 23:20:23 +0100

    Added 3d plots

--- Main/basic_1.py
+++ Main/basic_1.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
 
 regions = {'E12000001': 'North East', 'E12000002': 'North West', 'E12000003': 'Yorkshire and the Humber',
            'E12000004': 'East Midlands', 'E12000005': 'West Midlands', 'E12000006': 'East of England',
@@ -19,88 +22,88 @@ economic_acts = {1: "Employe", 2: "Self-employed", 3: "Unemployed", 4: "Full-tim
 
 
 def read_csv(file_name):
-    data = pd.read_csv(file_name, error_bad_lines=False)
-    data = data[data['Region'].isin(
+    df = pd.read_csv(file_name, error_bad_lines=False)
+    df = df[df['Region'].isin(
         ['E12000001', 'E12000002', 'E12000003', 'E12000004', 'E12000005',
          'E12000006', 'E12000007', 'E12000008', 'E12000009', 'W92000004'])]
-    data = data[data['Residence Type'].isin(['C', 'H'])]
-    data = data[data['Family Composition'].isin([1, 2, 3, 4, 5, 6, -9])]  # can change to: list(range(0, x).append(-9)
-    data = data[data['Population Base'].isin([1, 2, 3])]
-    data = data[data['Sex'].isin([1, 2])]
-    data = data[data['Age'].isin([1, 2, 3, 4, 5, 6, 7, 8])]
-    data = data[data['Marital Status'].isin([1, 2, 3, 4, 5])]
-    data = data[data['Student'].isin([1, 2])]
-    data = data[data['Country of Birth'].isin([1, 2, -9])]
-    data = data[data['Health'].isin([1, 2, 3, 4, 5, -9])]
-    data = data[data['Ethnic Group'].isin([1, 2, 3, 4, 5, -9])]
-    data = data[data['Religion'].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
-    data = data[data['Economic Activity'].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
-    data = data[data['Occupation'].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
-    data = data[data['Industry'].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, -9])]
-    data = data[data['Hours worked per week'].isin([1, 2, 3, 4, -9])]
-    data = data[data['Approximated Social Grade'].isin([1, 2, 3, 4, -9])]
-    data.duplicated(['Person ID'], keep='first')
-    return data
+    df = df[df['Residence Type'].isin(['C', 'H'])]
+    df = df[df['Family Composition'].isin([1, 2, 3, 4, 5, 6, -9])]  # can change to: list(range(0, x).append(-9)
+    df = df[df['Population Base'].isin([1, 2, 3])]
+    df = df[df['Sex'].isin([1, 2])]
+    df = df[df['Age'].isin([1, 2, 3, 4, 5, 6, 7, 8])]
+    df = df[df['Marital Status'].isin([1, 2, 3, 4, 5])]
+    df = df[df['Student'].isin([1, 2])]
+    df = df[df['Country of Birth'].isin([1, 2, -9])]
+    df = df[df['Health'].isin([1, 2, 3, 4, 5, -9])]
+    df = df[df['Ethnic Group'].isin([1, 2, 3, 4, 5, -9])]
+    df = df[df['Religion'].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
+    df = df[df['Economic Activity'].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
+    df = df[df['Occupation'].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, -9])]
+    df = df[df['Industry'].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, -9])]
+    df = df[df['Hours worked per week'].isin([1, 2, 3, 4, -9])]
+    df = df[df['Approximated Social Grade'].isin([1, 2, 3, 4, -9])]
+    df.duplicated(['Person ID'], keep='first')
+    return df
 
 
-def analyse(data):
-    print('Total no of records: ', data.shape[0])
-    print(data.groupby('Region').size(), '\n')
-    print(data.groupby('Residence Type').size(), '\n')
-    print(data.groupby('Family Composition').size(), '\n')
-    print(data.groupby('Population Base').size(), '\n')
-    print(data.groupby('Sex').size(), '\n')
-    print(data.groupby('Age').size(), '\n')
-    print(data.groupby('Marital Status').size(), '\n')
-    print(data.groupby('Student').size(), '\n')
-    print(data.groupby('Country of Birth').size(), '\n')
-    print(data.groupby('Health').size(), '\n')
-    print(data.groupby('Ethnic Group').size(), '\n')
-    print(data.groupby('Religion').size(), '\n')
-    print(data.groupby('Economic Activity').size(), '\n')
-    print(data.groupby('Occupation').size(), '\n')
-    print(data.groupby('Industry').size(), '\n')
-    print(data.groupby('Hours worked per week').size(), '\n')
-    print(data.groupby('Approximated Social Grade').size(), '\n')
+def analyse(df):
+    print('Total no of records: ', df.shape[0])
+    print(df.groupby('Region').size(), '\n')
+    print(df.groupby('Residence Type').size(), '\n')
+    print(df.groupby('Family Composition').size(), '\n')
+    print(df.groupby('Population Base').size(), '\n')
+    print(df.groupby('Sex').size(), '\n')
+    print(df.groupby('Age').size(), '\n')
+    print(df.groupby('Marital Status').size(), '\n')
+    print(df.groupby('Student').size(), '\n')
+    print(df.groupby('Country of Birth').size(), '\n')
+    print(df.groupby('Health').size(), '\n')
+    print(df.groupby('Ethnic Group').size(), '\n')
+    print(df.groupby('Religion').size(), '\n')
+    print(df.groupby('Economic Activity').size(), '\n')
+    print(df.groupby('Occupation').size(), '\n')
+    print(df.groupby('Industry').size(), '\n')
+    print(df.groupby('Hours worked per week').size(), '\n')
+    print(df.groupby('Approximated Social Grade').size(), '\n')
 
 
-def create_plot(char_type, data, column, names, title, x_axis, y_axis, legend):
-    data = data.replace({column: names})
-    plot = data[column].value_counts().plot(kind=char_type, title=title, legend=legend)
+def create_bar_plot(char_type, df, column, names, title, x_axis, y_axis):
+    df_size = len(df[column].unique())
+    color_map = [[x / df_size, x / (df_size * 5.0), 0.2] for x in range(df_size)]
+    df = df.replace({column: names})
+    plot = df[column].value_counts().plot(kind=char_type, title=title, color=color_map)
     plot.set_xlabel(x_axis)
     plot.set_ylabel(y_axis)
     plt.show()
 
 
-d = read_csv('data')
-analyse(d)
-create_plot('bar', d, 'Region', regions, 'Number of Records for each region', 'Regions', 'No. of Records', True)
-create_plot('bar', d, 'Occupation', occupations, 'Number of Records for each Occupation', 'Occupation', 'No. of Records', True)
-create_plot('pie', d, 'Age', ages, 'Distribution of the sample by age', "", "", False)
-create_plot('pie', d, 'Economic Activity', economic_acts, 'Distribution of the sample by the economic activity', "", "", False)
+def create_pie_plot(char_type, df, column, names, title):
+    df = df.replace({column: names})
+    df[column].value_counts().plot(kind=char_type, title=title)
+    plt.show()
 
 
+def create_3d_plot(df):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    plt.xticks([-9, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    plt.yticks(np.arange(10), df.index.levels[0])
+    for x in range(len(df.index.levels[0])):
+        xs = df[df.index.levels[0][x]]
+        ax.bar(xs.index, xs.values, zs=x, zdir='y')
+    plt.show()
 
 
+def group(df, col1, col2):
+    return df.groupby([col1, col2]).size()
 
 
+data = read_csv('data')
 
+# create_bar_plot('bar', data, 'Region', regions, 'Number of Records for each region', 'Regions', 'No. of Records')
+create_3d_plot(group(data, 'Region', 'Industry'))
 
-# print(data.groupby('Region').size()
-#
-# res_type = data.groupby('Residence Type')
-# family_compos = data.groupby('Family Composition')
-# population_base = data.groupby('Population Base')
-# sex = data.groupby('Sex')
-# age = data.groupby('Age')
-# marital_status = data.groupby('Marital Status')
-# student = data.groupby('Student')
-# birth_place = data.groupby('Country of Birth')
-# health = data.groupby('Health')
-# ethnic_group = data.groupby('Ethnic Group')
-# religion = data.groupby('Religion')
-# economic_activity = data.groupby('Economic Activity')
-# occupation = data.groupby('Occupation')
-# industry = data.groupby('Industry')
-# hours_per_week = data.groupby('Hours worked per week')
-# social_grade = data.groupby('Approximated Social Grade')
+# analyse(d)
+# create_bar_plot('bar', data, 'Occupation', occupations, 'Number of Records for each Occupation', 'Occupation', 'No. of Records')
+# create_pie_plot('pie', data, 'Age', ages, 'Distribution of the sample by age')
+# create_pie_plot('pie', data, 'Economic Activity', economic_acts, 'Distribution of the sample by the economic activity')
